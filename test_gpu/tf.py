19c5c58db0dcb6e0734927ad2bd227ea9354a35b
Manish Pushpam <mpritamp51@gmail.com>
2023-05-18 12:39:34 +0530

    Project Innit

---
+++ test_gpu/tf.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Create features
+X = np.array([-7.0, -4.0, -1.0, 2.0, 5.0, 8.0, 11.0, 14.0])
+
+# Create labels
+y = np.array([3.0, 6.0, 9.0, 12.0, 15.0, 18.0, 21.0, 24.0])
+
+# Visualize it
+plt.scatter(X, y)
